ac56e08e28a1ae28c33681da3adc0e9590b17c2e
Jacob Evarts <jevarts@uoregon.edu>
2020-10-27 16:37:17 -0700

    adding comments on possible parameters for calculator functions

--- Home.py
+++ Home.py
@@ -1,5 +1,16 @@
 # Calculate how much carbon is generated based on home energy usage
-home_footprt(time, home_size, heat_type):
+"""
+time:
+    the time frame in which to calculate; we could also just calculate for a
+    fixed amount of time (day, month, or year)
+home_type:
+    might be dorm, off-campus house, off-campus apartment
+home_size:
+    maybe a rough square footage
+heat_type:
+    probably more relevant for off-campus housing
+"""
+home_footprt(time, home_type, home_size, heat_type):
     pass
 
 if __name__ == '__main__':
