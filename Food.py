8924e342f8e094a7a0341179f8f6f527c4b9fa17
Jacob Evarts <jevarts@uoregon.edu>
2020-10-29 11:29:37 -0700

    skeleton functions in food.py and travel.py

--- Food.py
+++ Food.py
@@ -1,14 +1,20 @@
 # Calculate how much carbon is generated based on food consumption
 """
-time:
-    the time frame in which to calculate; we could also just calculate for a
-    fixed amount of time (day, month, or year)
 diet_type:
     maybe of a few options such as vegan, vegetarian, etc. or we could do an
     option of how many times per week you eat meat
 """
-def food_footprt(time, diet_type):
-    pass
+def food_footprt(diet_type):
+    if diet_type == "vegan":
+        pass
+    elif diet_type == "vegetarian":
+        pass
+    elif diet_type == "low_meat":
+        pass
+    elif diet_type == "med_meat":
+        pass
+    elif diet_type == "high_meat":
+        pass
 
 if __name__ == '__main__':
     food_footprt()
