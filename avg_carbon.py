ac99e8a465f8bf0dbe42ecf51eb79bd12a88f0a0
Jacob Evarts <jevarts@uoregon.edu>
2020-11-21 12:59:32 -0800

    Updated graphing in avg_carbon

--- avg_carbon.py
+++ avg_carbon.py
@@ -1,39 +1,47 @@
 #Graphs the user's carbon against a baseline average
-AVG_CARBON = 5000
+AVG_CARBON = 6370.09
+DAILY_AVG = 17.45
+
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from datetime import datetime
 
 def avg_carbon(usr_carbon, userID):
-    print(usr_carbon)
-    plt.figure(figsize=(4,6))
-    plt.axhline(y = AVG_CARBON, color = 'r', linestyle='--', linewidth=2)
-    plt.axhline(y = usr_carbon, color = 'b', linestyle='-', linewidth=3)
+    dcarbon = []; dates = []
+    for c in usr_carbon:
+        dcarbon.append(c[0])
+        date = datetime.strptime(c[1], '%Y-%m-%d')
+        dates.append(date)
 
-    plt.tick_params(
-        axis='x',
-        which='both',
-        bottom=False,
-        top=False,
-        labelbottom=False
-    )
-    #plt.title("How you compare against the average college student")
-    plt.ylabel("lbs. of CO2 per year")
+    ax = plt.gca()
+    formatter = mdates.DateFormatter("%Y-%m-%d")
+    ax.xaxis.set_major_formatter(formatter)
+    locator = mdates.DayLocator()
+    ax.xaxis.set_major_locator(locator)
+    plt.plot(dates, dcarbon, linewidth=2, label="Your carbon")
+
+    ax.axhline(DAILY_AVG, linewidth=2, linestyle='--', color = 'r', label="Average carbon")
+    plt.title("Your carbon vs. the average college student in a dorm")
+    plt.ylabel("lbs. of CO2 per day")
+    plt.xlabel("Date")
 
     path = "static/figures/"+userID+"_avg_carbon"#added username to create folders for user for syconicity
     plt.savefig(path, bbox_inches='tight', transparent=True)
-    #plt.show()
+    plt.show()
 
 def avg_carbon_str(usr_carbon):
-    diff = AVG_CARBON - usr_carbon
-    comp = " the average college student"
+    diff = round(AVG_CARBON - usr_carbon, 2)
+    comp = " the average college student living in a dorm"
     if diff == 0:
         return "You use the same amount of carbon as" + comp
     elif diff < 0:
-        return "You use " +str(abs(diff))+ " more lbs of co2 per year than" + comp
+        return "You use " +str(abs(diff))+ " more lbs of CO2/yr. than" + comp
     else:
-        return "You use " +str(abs(diff))+ " less lbs of co2 per year than" + comp
+        return "You use " +str(abs(diff))+ " less lbs of CO2/yr. than" + comp
 
 if __name__ == '__main__':
     # Unit test
+    avg_carbon([[18.81, '2020-11-20'], [18.71, '2020-11-21']], "-1")
     print(avg_carbon_str(5000))
     print(avg_carbon_str(500))
     print(avg_carbon_str(5001))
